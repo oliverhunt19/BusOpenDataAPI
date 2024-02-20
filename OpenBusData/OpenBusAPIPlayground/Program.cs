444ca95541aafe508ee6ae11800c7a10888c3462
Oliver Hunt <oliver.hunt@renishaw.com>
2024-02-20 16:11:08 +0000

    added async timer

--- OpenBusData/OpenBusAPIPlayground/Program.cs
+++ OpenBusData/OpenBusAPIPlayground/Program.cs
@@ -7,11 +7,23 @@ string APIKey = "df3191ca1da34bb25d2072f60067eee9c00e2b22";
 BusLocationRequest busLocationRequest = new BusLocationRequest()
 {
     Key = APIKey,
-    Bounds = LatLngBounds.GetBoundingBox(new LatLng(50.723344, -3.595641),10),
-    //LineRef = "1",
+    Bounds = LatLngBounds.GetBoundingBox(new LatLng(50.824217, -3.423283),10),
+    LineRef = "1",
 };
 
 BusLocationAPI busLocationAPI = new BusLocationAPI();
 var resp = await busLocationAPI.QueryAsync(busLocationRequest);
 
-Console.WriteLine();
+async Task RunLocationIdent(CancellationToken cancellationToken)
+{
+    BusLocationResponce resp = await busLocationAPI.QueryAsync(busLocationRequest, cancellationToken);
+    Console.Write(resp.ServiceDelivery.VehicleMonitoringDelivery.FirstOrDefault()?.MonitoredVehicleJourney?.VehicleLocation.Latitude + " ");
+    Console.WriteLine(resp.ServiceDelivery.VehicleMonitoringDelivery.FirstOrDefault()?.MonitoredVehicleJourney?.VehicleLocation.Longitude);
+}
+
+TimerAsync timerAsync = new TimerAsync(RunLocationIdent,TimeSpan.Zero, TimeSpan.FromSeconds(30));
+timerAsync.Start();
+Console.ReadLine();
+await timerAsync.Stop();
+Console.WriteLine("Stopped");
+Console.ReadLine();
